941c259aa817e58d4d54dd2e20697fa2c99c185c
matheusmedeirosn <matheusmedeiroosn@gmail.com>
2024-01-14 14:37:18 -0300

    Permitindo adição cors

--- src/main/java/br/com/centralagendamento/controller/AgendamentoController.java
+++ src/main/java/br/com/centralagendamento/controller/AgendamentoController.java
@@ -12,6 +12,7 @@ import java.time.LocalTime;
 import java.util.List;
 
 @RestController
+@CrossOrigin(origins = "http://127.0.0.1:5500")
 @RequestMapping("/api/agendamentos")
 public class AgendamentoController {
 
